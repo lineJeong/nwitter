c589c421da739e7aef16f178eabd677cb6f8d04a
lineJeong <line.jeong32@gmail.com>
2022-08-05 22:42:38 +0900

    2.3 Creating Account

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
-import React from "react";
-import AppRouter from "./Router";
+import React, { useState } from "react";
+import AppRouter from "components/AppRouter";
+import { authService } from "fbase";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+
   return (
     <div>
-      <AppRouter />
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; Nwitter {new Date().getFullYear()}</footer>
     </div>
   );
 }
--- src/fbase.js
+++ src/fbase.js
@@ -1,4 +1,5 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -10,5 +11,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
-export default app;
+export const authService = getAuth();
